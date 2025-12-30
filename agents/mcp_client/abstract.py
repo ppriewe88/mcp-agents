9dbf01021001f18019eefc39813eff6f7afbbaed
patrick.priewe <ppriewe@gmx.net>
2025-12-30 22:15:42 +0100

    feat: setting up abstract client

---
+++ agents/mcp_client/abstract.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from mcp.types import CallToolResult
+
+from agents.mcp_client.models import MCPToolDecision, OpenAITool
+
+
+class BaseMCPClient(ABC):
+    """Model for abstract mcp client. Serves to define signatures."""
+
+    @abstractmethod
+    def __init__(self):
+        """Constructor."""
+        raise NotImplementedError
+
+    @abstractmethod
+    async def connect(self) -> bool:
+        """Method to connect to mcp server."""
+        raise NotImplementedError
+
+    @abstractmethod
+    async def close(self) -> bool:
+        """Method to close connection to mcp server."""
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_tools(self) -> List[OpenAITool]:
+        """Method to get tools from mcp server."""
+        raise NotImplementedError
+
+    @abstractmethod
+    async def call_tools(self, tooling_decision: List[MCPToolDecision]) -> List[CallToolResult]:
+        """Method to call tools on mcp server."""
+        raise NotImplementedError
